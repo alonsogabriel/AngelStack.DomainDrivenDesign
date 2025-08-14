23171f739fcc98db49c24c407c06416941877b64
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-14 18:54:25 -0300

    Rename project and directories

--- DomainDrivenDesign.DomainObjects/Constants/CountryCodes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DomainDrivenDesign.DomainObjects.Constants;
-
-public static class CountryCodes
-{
-    public const int UnitedStates = 1;
-    public const int Brazil = 55;
-}
--- DomainDrivenDesign.DomainObjects/Interfaces/IPhoneNumberFormatter.cs
+++
@@ -1,8 +0,0 @@
-﻿using DomainDrivenDesign.DomainObjects.ValueObjects;
-
-namespace DomainDrivenDesign.DomainObjects.Interfaces;
-
-public interface IPhoneNumberFormatter
-{
-    string Format(PhoneNumber number);
-}
