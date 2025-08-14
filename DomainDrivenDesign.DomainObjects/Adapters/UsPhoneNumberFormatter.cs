be452f30a98e04105a5a04d1bac95c23d90f1f94
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-14 14:34:15 -0300

    Add country code validation to phone number formatters

--- DomainDrivenDesign.DomainObjects/Adapters/UsPhoneNumberFormatter.cs
+++ DomainDrivenDesign.DomainObjects/Adapters/UsPhoneNumberFormatter.cs
@@ -1,4 +1,6 @@
-﻿using DomainDrivenDesign.DomainObjects.Interfaces;
+﻿using DomainDrivenDesign.Abstractions.Extensions;
+using DomainDrivenDesign.DomainObjects.Constants;
+using DomainDrivenDesign.DomainObjects.Interfaces;
 using DomainDrivenDesign.DomainObjects.ValueObjects;
 
 namespace DomainDrivenDesign.DomainObjects.Adapters;
@@ -7,6 +9,12 @@ internal class UsPhoneNumberFormatter : IPhoneNumberFormatter
 {
     public string Format(PhoneNumber number)
     {
+        number.AreaCode.Guard(new ArgumentNullException(nameof(number.AreaCode)));
+        if (number.CountryCode.Value is not CountryCodes.UnitedStates)
+        {
+            throw new ArgumentException("Invalid country code.", nameof(number.CountryCode));
+        }
+
         var countryCode = number.CountryCode.Value; // Should be "1" for US
         var areaCode = number.AreaCode!.Value;
 
