3209e337b589946a334dd7eeb26c5050b1f54456
Gabriel Alonso <gabriel.tupi.alonso@gmail.com>
2025-08-15 23:42:25 -0300

    Refact CnhNumber and IssuingAuthority objects

--- AngelStack.DomainDrivenDesign/ValueObjects/Brazil/IssuingAuthority.cs
+++ AngelStack.DomainDrivenDesign/ValueObjects/Brazil/IssuingAuthority.cs
@@ -1,13 +1,9 @@
-﻿using AngelStack.DomainDrivenDesign.Abstractions;
+﻿using AngelStack.Common.Strings;
+using AngelStack.DomainDrivenDesign.Abstractions;
 
 namespace AngelStack.DomainDrivenDesign.ValueObjects.Brazil;
 
-// [StringValidation(MinLength = MIN_LENGTH, MaxLength = MAX_LENGTH)]
-public record IssuingAuthority : StringValidatable
-{
-    public const int MIN_LENGTH = 1;
-    public const int MAX_LENGTH = 20;
-    public IssuingAuthority(string value) : base(value)
-    {
-    }
-}
+[Required]
+[MinLength(1)]
+[MaxLength(40)]
+public record IssuingAuthority(string Value) : StringValidatable(Value);
